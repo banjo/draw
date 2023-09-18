bd48e881a1b09e670bb8706595c54a703120ae3e
Anton Ödman <anton.odman@gmail.com>
2023-09-18 21:40:11 +0200

    chore: update to latest npm versions

--- apps/web/src/routes/error-page.tsx
+++ apps/web/src/routes/error-page.tsx
@@ -6,7 +6,7 @@ const ErrorPage: React.FC = () => {
     let errorMessage: string;
 
     if (isRouteErrorResponse(error)) {
-        errorMessage = error.error?.message || error.statusText;
+        errorMessage = error.statusText;
     } else if (error instanceof Error) {
         errorMessage = error.message;
     } else if (typeof error === "string") {
