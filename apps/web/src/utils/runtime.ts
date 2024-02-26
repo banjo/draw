3821ae0b78be362941b353a37f3152ec07619658
Anton Ödman <anton.odman@gmail.com>
2024-02-26 13:47:39 +0100

    chore: fix trpc provider in web

--- apps/web/src/utils/runtime.ts
+++ apps/web/src/utils/runtime.ts
@@ -2,6 +2,6 @@ import { raise } from "@banjoanton/utils";
 
 export const isDev = () => import.meta.env.DEV && import.meta.env.VITE_LOCAL_DEVELOPMENT === "true";
 
-export const getUrl = () => {
+export const getApiUrl = () => {
     return import.meta.env.VITE_API_URL ?? raise("VITE_API_URL is not defined");
 };
