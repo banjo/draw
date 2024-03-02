9096819e59c1b9da5b463f3cd818226e176b90ef
Anton Ödman <anton.odman@gmail.com>
2024-03-02 18:02:54 +0100

    chore: error data might be optional

--- apps/web/src/providers/trpc-provider.tsx
+++ apps/web/src/providers/trpc-provider.tsx
@@ -60,7 +60,7 @@ export const TrpcProvider: FC<PropsWithChildren> = ({ children }) => {
                         retry(failureCount, error) {
                             if (error instanceof TRPCClientError) {
                                 if (
-                                    error.data.code === "UNAUTHORIZED" &&
+                                    error.data?.code === "UNAUTHORIZED" &&
                                     error.shape?.cause === Cause.EXPIRED_TOKEN
                                 ) {
                                     refreshToken(); // not best solution, but it works
