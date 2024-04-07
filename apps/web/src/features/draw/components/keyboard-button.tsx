9a33d4097b74c791204cf1c5e61a5a599cfa4316
Anton Ödman <anton.odman@gmail.com>
2024-04-07 17:44:53 +0200

    chore: do not select keyboard button

--- apps/web/src/features/draw/components/keyboard-button.tsx
+++ apps/web/src/features/draw/components/keyboard-button.tsx
@@ -12,6 +12,7 @@ export const KeyboardButton = ({ onClick, children, className }: Props) => {
             onClick={onClick}
             className={cn(
                 "flex items-center justify-center rounded-lg border-2 bg-[#FAFBFC] border-[#CED4DB] py-1 px-2",
+                "select-none",
                 className
             )}
         >
