cb14bcdef479ca8c478f4cec8f74ec9539819d78
Anton Ödman <anton.odman@gmail.com>
2024-04-13 13:37:47 +0200

    chore: remove console log

--- apps/web/src/features/selected-element-visuals/components/extend-elements-container.tsx
+++ apps/web/src/features/selected-element-visuals/components/extend-elements-container.tsx
@@ -123,8 +123,6 @@ export const ExtendElementsContainer = ({ refs }: Props) => {
             return arrow;
         });
 
-        console.log({ updatedArrow });
-
         const updatedElements = ElementUtil.mergeElements(elements, [updatedArrow]);
         excalidrawApi.updateScene({
             elements: updatedElements,
