9772d737cdff90d2275bfb6c9aa3514ccb7369fd
Anton Ödman <anton.odman@gmail.com>
2024-04-21 21:59:52 +0200

    chore: rename context menu labels

--- apps/web/src/features/draw/hooks/base/use-context-menu.ts
+++ apps/web/src/features/draw/hooks/base/use-context-menu.ts
@@ -1,22 +1,35 @@
+import { useGlobal } from "@/contexts/global-context";
 import { useExport } from "@/features/draw/hooks/base/use-export";
 import { NativeContextMenu } from "@/features/draw/models/native/native-context-menu";
+import { ElementUtil } from "@/features/draw/utils/element-util";
 
 export const useContextMenu = () => {
     const { exportPngToClipboard, exportSvgToClipboard } = useExport();
+    const { excalidrawApi } = useGlobal();
 
     const onContextMenu = async (e: React.MouseEvent) => {
         // allow native context menu to show
         setTimeout(() => {
+            if (!excalidrawApi) return;
+            const appState = excalidrawApi.getAppState();
+            const elements = excalidrawApi.getSceneElements();
+
+            const hasSelection = ElementUtil.getSelectedElements(appState, elements);
+
             NativeContextMenu.parse();
             NativeContextMenu.updateEntry({
                 dataTestId: "copyAsPng",
-                label: "Copy to clipboard as PNG",
+                label: hasSelection
+                    ? "Copy selection to clipboard as PNG"
+                    : "Copy to clipboard as PNG",
                 onClick: exportPngToClipboard,
             });
 
             NativeContextMenu.updateEntry({
                 dataTestId: "copyAsSvg",
-                label: "Copy to clipboard as SVG",
+                label: hasSelection
+                    ? "Copy selection to clipboard as SVG"
+                    : "Copy to clipboard as SVG",
                 onClick: exportSvgToClipboard,
             });
         }, 0);
