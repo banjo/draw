9982fb16839f751f83bfd182a5356834ff0da709
Anton Ödman <anton.odman@gmail.com>
2025-05-19 16:33:21 +0200

    chore: remove boring eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,11 @@
 import defaultConfig from "@banjoanton/eslint-config";
 
 /** @type {import("eslint").Linter.Config} */
-export default [...defaultConfig];
+export default [
+    ...defaultConfig,
+    {
+        rules: {
+            "react/forbid-component-props": "off",
+        },
+    },
+];
